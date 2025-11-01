852e752d7101b66671e7e34d7267718d0872a248
farhan0167 <afi225@nyu.edu>
2025-11-01 11:04:50 -0400

    add phoenix integration

--- plan_exec/config.py
+++ plan_exec/config.py
@@ -6,6 +6,7 @@ load_dotenv()
 class Config(BaseSettings):
     OPENAI_API_KEY: str
     TAVILY_API_KEY: str
+    PHOENIX_COLLECTOR_ENDPOINT: str = "http://localhost:4317"
     
     
 config = Config()
--- plan_exec/main.py
+++ plan_exec/main.py
@@ -1,14 +1,20 @@
 from langchain.messages import HumanMessage
+from openinference.instrumentation.langchain import LangChainInstrumentor
+from phoenix.otel import register
 
 from agent import create_agent
+
+tracer_provider = register(project_name="plan_exec")
+LangChainInstrumentor().instrument(tracer_provider=tracer_provider)
 
 
 def main(user_message):
     agent = create_agent()
     
     state = {
-        "messages": [HumanMessage(content=user_message)],
-        "todo": []
+        "user_message": user_message,
+        "todo": [],  # List[Item]
+        "past_steps": []  # List[AnyMessage]
     }
     
     response = agent.invoke(state)
@@ -20,3 +26,5 @@ if __name__ == "__main__":
     user_message = input("What would you like to know? ")
     response = main(user_message)
     print(response)
+    # agent = create_agent()
+    # agent.get_graph().draw_mermaid_png(output_file_path="./graph.png")
